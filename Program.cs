8ec3ac2e20153de4fd9a822bdd16533397ad231c
Samuel de Andrade <52175846+SamDoing@users.noreply.github.com>
2022-08-29 23:37:53 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -42,7 +42,7 @@ namespace Stone
             return salarioTipo;
         }
 
-        //Stack overflow by recursion vunerable. Here happens ~9k2 call's
+        //Stack overflow by recursion vunerable. Here happens at ~9k2 call
         static double GetSalario()
         {
             Console.Write("Quanto o funcionario ganha por mes: ");
